ef28410157964e811bb4c576a4d9cd34017d3d4e
Krishna Thaker <Krishna Thaker>
2016-11-27 21:11:27 -0600

    add ex5 sources

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  // Or, to start a slideshow, just pass in an array of images
+  $("#main-wrapper").backstretch([
+    "images/dublin1.jpg",
+    "images/dublin3.jpg",
+    "images/edinburgh1.jpg",
+    "images/galway1.jpg",
+    "images/galway2.jpg",
+    "images/galway3.jpg",
+    "images/galway4.jpg",
+    "images/galway5.jpg",
+    "images/glasgow2.jpg",
+    "images/glenfinnan1.jpg",
+    "images/london3.jpg",
+    "images/london1.jpg",
+    "images/london2.jpg"
+  ], {duration: 4000});
+});
