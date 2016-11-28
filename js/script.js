de4e3d6d6634c0e89eb9a14a9df86115e3d818ff
Krishna Thaker <Krishna Thaker>
2016-11-27 21:17:07 -0600

    fix file extensions

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 $(document).ready(function(){
   // Or, to start a slideshow, just pass in an array of images
   $("#main-wrapper").backstretch([
-    "images/dublin1.jpg",
-    "images/dublin3.jpg",
-    "images/edinburgh1.jpg",
-    "images/galway1.jpg",
-    "images/galway2.jpg",
-    "images/galway3.jpg",
-    "images/galway4.jpg",
-    "images/galway5.jpg",
-    "images/glasgow2.jpg",
-    "images/glenfinnan1.jpg",
-    "images/london3.jpg",
-    "images/london1.jpg",
-    "images/london2.jpg"
+    "images/dublin1.JPG",
+    "images/dublin3.JPG",
+    "images/edinburgh1.JPG",
+    "images/galway1.JPG",
+    "images/galway2.JPG",
+    "images/galway3.JPG",
+    "images/galway4.JPG",
+    "images/galway5.JPG",
+    "images/glasgow2.JPG",
+    "images/glenfinnan1.JPG",
+    "images/london3.JPG",
+    "images/london1.JPG",
+    "images/london2.JPG"
   ], {duration: 4000});
 });
